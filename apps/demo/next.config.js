6e471ce84779591731133aa2784a53375b02bb85
Vedant Chainani <vedantchainani1084@gmail.com>
2024-04-03 13:18:08 +0530

    chore: use rewrites in config

--- apps/demo/next.config.js
+++ apps/demo/next.config.js
@@ -1,21 +1,23 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./src/env.js");
+await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
+  async rewrites() {
+    return [
+      {
+        source: '/docs',
+        destination: 'https://react-terminal-docs.vercel.app',
+      },
+      {
+        source: '/docs/:slug*',
+        destination: 'https://react-terminal-docs.vercel.app/:slug*',
+      },
+    ];
+  },
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
 };
 
